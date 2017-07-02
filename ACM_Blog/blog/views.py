f590b578c379fd3d32fc82e6b3bebebb317d7c1f
priyank jairaj <f2016615@pilani.bits-pilani.ac.in>
2017-07-02 13:51:53 +0530

    basic site login is made, fixed bugs

--- ACM_Blog/blog/views.py
+++ ACM_Blog/blog/views.py
@@ -6,31 +6,35 @@ from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login, logout
 from django.views import generic
 from django.views.generic import View
+from django.contrib.auth.models import User
 
 def post_list(request):
-    if request.user.is_authenticated():
+    #if request.user.is_authenticated():
+        users= User.objects.all()
         posts = Post.objects.all().order_by('published_date')
-        return render(request, 'blog/post_list.html', {'posts': posts})
-    if not request.user.is_authenticated():
-        return render(request, 'blog/login.html')
+        return render(request, 'blog/post_list.html', {'posts': posts,'users':users})
+    #if not request.user.is_authenticated():
+        #return render(request, 'blog/login.html')
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+     if request.user.is_authenticated():
+        post = get_object_or_404(Post, pk=pk)
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+                form = PostForm(instance=post)
+                return render(request, 'blog/post_edit.html', {'form': form})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -47,11 +51,7 @@ def post_new(request):
 
 def logout_user(request):
     logout(request)
-    form = UserForm(request.POST or None)
-    context = {
-        "form": form,
-    }
-    return render(request, 'blog/login.html', context)
+    return redirect('post_list')
 
 
 def login_user(request):
@@ -62,8 +62,7 @@ def login_user(request):
         if user is not None:
             if user.is_active:
                 login(request, user)
-                posts = Post.objects.all().order_by('published_date')
-                return render(request, 'blog/post_list.html', {'posts': posts})
+                return redirect('post_list')
             else:
                 return render(request, 'blog/login.html', {'error_message': 'Your account has been disabled'})
         else:
@@ -83,9 +82,6 @@ def register(request):
         if user is not None:
             if user.is_active:
                 login(request, user)
-                posts = Post.objects.all().order_by('published_date')
-                return render(request, 'blog/post_list.html', {'posts': posts})
-    context = {
-        "form": form,
-    }
-    return render(request, 'blog/register.html', context)
+                return redirect('post_list')
+
+    return render(request, 'blog/register.html',{'form':form})
